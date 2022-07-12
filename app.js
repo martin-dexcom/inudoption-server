108c5da09d2f5027f235be7ec0b8c33bca757c33
DanielDexcom <dm0520@dexcom.com>
2022-07-12 01:51:26 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const axios = require('axios').default;
+const app = express()
+const port = 3000
+var fs = require('fs');
+
+app.use(express.json()) // for parsing application/json
+
+const animals = ['Dog', 'Cat', 'Bird']
+
+app.get('/randomAnimal', (req, res) => {
+  var array = JSON.parse(fs.readFileSync('pet-names.json', 'utf8'));
+  let i = randomIndex(array.length - 1)
+  let randAnimalIndex = randomIndex(animals.length - 1)
+  const dob = randomDate(new Date(2012, 0, 1), new Date());
+  var animal = animals[randAnimalIndex]
+  console.log(animal)
+
+  if (animal == 'Cat') {
+  axios.get('https://some-random-api.ml/animal/cat')
+    .then(function (response) {
+      var dog = { 'name': array[i], 'image': response.data.image, 'animal': 'Cat', 'fact': response.data.fact, 'dateOfBirth': dob}
+      res.json(dog)
+    })
+    .catch(function (error) {
+      res.status(500).jsonp({ error })
+    })
+}
+if (animal == 'Dog') {
+  axios.get('https://some-random-api.ml/animal/dog')
+    .then(function (response) {
+      var dog = { 'name': array[i], 'image': response.data.image, 'animal': 'Dog', 'fact': response.data.fact, 'dateOfBirth': dob}
+      res.json(dog)
+    })
+    .catch(function (error) {
+      res.status(500).jsonp({ error })
+    })
+  }
+
+  if (animal == 'Bird') {
+    axios.get('https://some-random-api.ml/animal/bird')
+      .then(function (response) {
+        var dog = { 'name': array[i], 'image': response.data.image, 'animal': 'Bird', 'fact': response.data.fact, 'dateOfBirth': dob}
+        res.json(dog)
+      })
+      .catch(function (error) {
+        res.status(500).jsonp({ error })
+      })
+  }
+})
+
+const randomIndex = (maxIndex) => Math.floor(Math.random() * (maxIndex + 1))
+const randomDate = (start, end) => new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+
+const d = randomDate(new Date(2012, 0, 1), new Date());
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
